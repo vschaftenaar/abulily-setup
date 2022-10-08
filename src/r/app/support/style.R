e503fe9b4358418adf550ad91cf281e4a5fa500d
vschaftenaar <4079204+vschaftenaar@users.noreply.github.com>
2022-10-08 14:08:57 +0200

    style upgrade

--- src/r/app/support/style.R
+++ src/r/app/support/style.R
@@ -5,16 +5,16 @@ fontFamily <-  'bahn'
 # fontFamily <-  'mono'
 
 windowsFonts(arial = "Arial Unicode MS",
-       arialBlack='Arial Black',
-       lucidia='Lucida Console',
-       arialNarrow='Arial Narrow',
-       mono = "Courier New",
-       calibri = "Calibri",
-       calibriLight = "Calibri light",
-       gothic = "MS Gothic",
-       corbelLight='Corbel Light',
-       bahn='Bahnschrift Light'
-       )
+             arialBlack='Arial Black',
+             lucidia='Lucida Console',
+             arialNarrow='Arial Narrow',
+             mono = "Courier New",
+             calibri = "Calibri",
+             calibriLight = "Calibri light",
+             gothic = "MS Gothic",
+             corbelLight='Corbel Light',
+             bahn='Bahnschrift Light'
+)
 
 
 palette <- data.table(
@@ -39,8 +39,8 @@ palette <- data.table(
 palette <- data.table(
   bg            = '#f8f7f7',
   panel         = '#647886',
-  txt           = '#647886',
-  panel.txt     = '#000000',
+  txt           = '#536470',
+  panel.txt     = '#536470',
   fg.0          = '#647886',
   fg.0.fade     = '#64788615',
   fg.1          = '#8CC6D0',
@@ -52,7 +52,11 @@ palette <- data.table(
   green         = '#8CC6D0',
   dropdown      = '#f6f6f6',
   side.panel.bg = '#efeded90',
-  caret         = '#f8f7f7'
+  caret         = '#f8f7f7',
+  dt.head       = '#f8f7f7',
+  h1            = '#536470',
+  h2            = '#536470',
+  h3            = '#536470'
 )
 
 
@@ -67,18 +71,18 @@ getStyle <- function(){
   
   tagList <- list(
     #font
-       tags$head(tags$style(paste0('* {font-family: ',windowsFonts(fontFamily),';font-size:105%;color:',palette$txt,'}')))
-      ,tags$head(tags$style(paste0('ul{font-family: ',windowsFonts(fontFamily),';font-size:95%;color:',palette$txt,'}')))
-       # ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';}')))
-       # ,tags$head(tags$style(paste0('h1{font-family: ',windowsFonts(fontFamily),';}')))
-       ,tags$head(tags$style(paste0('h1{font-family: ',windowsFonts(fontFamily),';color:',palette$txt,'}')))
-       ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';color:',palette$txt,'}')))
-       ,tags$head(tags$style(paste0('h3{font-family: ',windowsFonts(fontFamily),';color:',palette$txt,'}')))
-       # ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';font-size:100%}')))
-       
-      ,tags$head(tags$style(paste0('.form-control{color:',palette$txt,';}')))
-      ,tags$head(tags$style(paste0('.caret{color:',palette$caret,';}')))
-
+    tags$head(tags$style(paste0('* {font-family: ',windowsFonts(fontFamily),';font-size:105%;color:',palette$txt,'}')))
+    ,tags$head(tags$style(paste0('ul{font-family: ',windowsFonts(fontFamily),';font-size:95%;color:',palette$txt,'}')))
+    # ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';}')))
+    # ,tags$head(tags$style(paste0('h1{font-family: ',windowsFonts(fontFamily),';}')))
+    ,tags$head(tags$style(paste0('h1{font-family: ',windowsFonts(fontFamily),';color:',palette$h1,'}')))
+    ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';color:',palette$h2,'}')))
+    ,tags$head(tags$style(paste0('h3{font-family: ',windowsFonts(fontFamily),';color:',palette$h3,'}')))
+    # ,tags$head(tags$style(paste0('h2{font-family: ',windowsFonts(fontFamily),';font-size:100%}')))
+    
+    ,tags$head(tags$style(paste0('.form-control{color:',palette$txt,';}')))
+    ,tags$head(tags$style(paste0('.caret{color:',palette$caret,';}')))
+    
     # navbar  
     ,tags$style(HTML('.nav-tabs {border-bottom: 1px solid #dfdfdf}'))
     ,tags$style(HTML(paste0(
@@ -90,14 +94,14 @@ getStyle <- function(){
      .nav-tabs>li.active>a:active:hover,
      .nav-tabs>li.active>a:focus:hover
         {color:',palette$panel.txt,';background-color:','red',';box-shadow: inset 0 -1px 0 ',palette$panel,';}')))
-
+    
     ,tags$style(HTML('.container-fluid {
        margin-right: auto;
        margin-left: auto;
        padding-left: 0px;
        padding-right: 0px;
      }'))
-
+    
     ,tags$style(HTML(paste0(
       ".navbar-default .navbar-nav>.open>a,
        .navbar-default .navbar-nav>.open>a:hover,
@@ -130,59 +134,60 @@ getStyle <- function(){
     ,tags$style(HTML(paste0('.navbar-default .navbar-brand {background-color:',palette$panel,';color:',palette$bg,';height: 70px;}')))
     
     
-
+    
     ,tags$style(HTML(paste0('.dropdown-menu>.active>a {background-color:',palette$panel,'10;color:',palette$panel.txt,';}')))
     
     
     #plots
-      ,tags$style(type="text/css",".recalculating { opacity: 1.0; }")
+    ,tags$style(type="text/css",".recalculating { opacity: 1.0; }")
     
     
     #slider
-      ,tags$style(HTML(paste0(".irs--shiny .irs-single, .irs--shiny .irs-from, .irs--shiny .irs-to {color: ",palette$txt," ;background-color:",palette$bg,";}")))
-      ,tags$style(HTML(paste0(".irs--shiny .irs-min, .irs--shiny .irs-max {color:",palette$bg,";background-color:",palette$bg,";}")))
-      ,tags$style(HTML(paste0(".irs--shiny .irs-bar {border: ",palette$fg.0," 5px solid ;background:",palette$fg.0,"}")))
+    ,tags$style(HTML(paste0(".irs--shiny .irs-single, .irs--shiny .irs-from, .irs--shiny .irs-to {color: ",palette$txt," ;background-color:",palette$bg,";}")))
+    ,tags$style(HTML(paste0(".irs--shiny .irs-min, .irs--shiny .irs-max {color:",palette$bg,";background-color:",palette$bg,";}")))
+    ,tags$style(HTML(paste0(".irs--shiny .irs-bar {border: ",palette$fg.0," 5px solid ;background:",palette$fg.0,"}")))
     
-      
+    
     #character input
-      ,tags$style(HTML(paste0("input[type=text].form-control:focus {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0("input[type=text].form-control:hover {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0("input[type=text].form-control       {box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))  
-      ,tags$style(HTML(paste0("input[type=text].form-control:focus {border: solid 1px ",palette$fg.0,"10}")))
-      ,tags$style(HTML(paste0("input[type=text].form-control:hover {border: solid 1px",palette$fg.0,"30}")))
-      ,tags$style(HTML(paste0("input[type=text].form-control       {border: solid 1px  ",palette$fg.0,"10}")))
-      ,tags$style(HTML(paste0("input[type=text].form-control       {padding-left: 10px}")))
-      
-      
+    ,tags$style(HTML(paste0("input[type=text].form-control:focus {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0("input[type=text].form-control:hover {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0("input[type=text].form-control       {box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))  
+    ,tags$style(HTML(paste0("input[type=text].form-control:focus {border: solid 1px ",palette$fg.0,"10}")))
+    ,tags$style(HTML(paste0("input[type=text].form-control:hover {border: solid 1px",palette$fg.0,"30}")))
+    ,tags$style(HTML(paste0("input[type=text].form-control       {border: solid 1px  ",palette$fg.0,"10}")))
+    ,tags$style(HTML(paste0("input[type=text].form-control       {padding-left: 10px}")))
+    
+    
     #numerical input
-      ,tags$style(HTML(paste0("input[type=number].form-control:focus {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control:hover {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control       {box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control:focus {border: solid 1px",palette$fg.0,"10}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control:hover {border: solid 1px",palette$fg.0,"30}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control       {border: solid 1px ",palette$fg.0,"10}")))
-      ,tags$style(HTML(paste0("input[type=number].form-control       {padding-left: 10px}")))
-      
-      
+    ,tags$style(HTML(paste0("input[type=number].form-control:focus {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control:hover {box-shadow: 0 12px 5px -5px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control       {box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control:focus {border: solid 1px",palette$fg.0,"10}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control:hover {border: solid 1px",palette$fg.0,"30}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control       {border: solid 1px ",palette$fg.0,"10}")))
+    ,tags$style(HTML(paste0("input[type=number].form-control       {padding-left: 10px}")))
+    
+    
     #pickerInput
-      ,tags$style(HTML(paste0(".dropdown-menu>.active>a,.dropdown-menu>.active>a:hover,.dropdown-menu>.active>a:focus
+    ,tags$style(HTML(paste0(".dropdown-menu>.active>a,.dropdown-menu>.active>a:hover,.dropdown-menu>.active>a:focus
                               {color:",palette$bg,";text-decoration: none;outline: 0;background-color:",palette$fg.0,"}")))
     
-      ,tags$style(HTML(paste0(".btn{text-transform: none;border: solid 1px ",palette$fg.0,"10;box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))
-      ,tags$style(HTML(paste0(".btn:hover{text-transform: none;border: solid 1px ",palette$fg.0,"30;box-shadow: 0 12px 5px -5px ",col.shadow,"50;background-color:",palette$bg,"}")))
-      ,tags$style(HTML(paste0(".btn:focus{text-transform: none;border: solid 1px ",palette$fg.0,"60;box-shadow: 0 12px 5px -5px ",col.shadow,"50;background-color:",palette$bg,"}")))
-      ,tags$style(HTML(paste0(".open>.dropdown-toggle.btn{background-color:",palette$bg,"}")))
-      ,tags$style(HTML(paste0(".open>.dropdown-toggle.btn:hover{background-color:",palette$bg,"}")))
+    ,tags$style(HTML(paste0(".btn{text-transform: none;border: solid 1px ",palette$fg.0,"10;box-shadow: 0 8px 10px -10px ",col.shadow,"50}")))
+    ,tags$style(HTML(paste0(".btn:hover{text-transform: none;border: solid 1px ",palette$fg.0,"30;box-shadow: 0 12px 5px -5px ",col.shadow,"50;background-color:",palette$bg,"}")))
+    ,tags$style(HTML(paste0(".btn:focus{text-transform: none;border: solid 1px ",palette$fg.0,"60;box-shadow: 0 12px 5px -5px ",col.shadow,"50;background-color:",palette$bg,"}")))
+    ,tags$style(HTML(paste0(".open>.dropdown-toggle.btn{background-color:",palette$bg,"}")))
+    ,tags$style(HTML(paste0(".open>.dropdown-toggle.btn:hover{background-color:",palette$bg,"}")))
     
     #data table
-      ,tags$head(tags$style(paste0("table.dataTable tbody tr.even  {color:",palette$txt,";background-color:",palette$bg," !important;}")))
-      ,tags$head(tags$style(paste0("table.dataTable tbody tr.odd   {color:",palette$txt,";background-color:",palette$fg.0.fade," !important;}")))
-      ,tags$head(tags$style(paste0("table.dataTable thead tr       {color:",palette$bg,"; background-color:",palette$fg.0," !important;}")))
+    ,tags$head(tags$style(paste0("table.dataTable tbody tr.even  {color:",palette$txt,";background-color:",palette$bg," !important;}")))
+    ,tags$head(tags$style(paste0("table.dataTable tbody tr.odd   {color:",palette$txt,";background-color:",palette$fg.0.fade," !important;}")))
+    ,tags$head(tags$style(paste0("table.dataTable thead tr       {color:",palette$bg,"; background-color:",palette$fg.0," !important;}")))
+    ,tags$head(tags$style(paste0("table.dataTable thead *        {color:",palette$dt.head," !important;}")))
     
     
     #panel
-      ,tags$style(HTML(paste0(".panel-default>.panel-heading {color: ",palette$bg,";background-color:",palette$fg.0,";border-color: red;}")))
-      ,tags$style(HTML(paste0(".panel-body {background-color: ",palette$side.panel.bg,";}")))
+    ,tags$style(HTML(paste0(".panel-default>.panel-heading {color: ",palette$bg,";background-color:",palette$fg.0,";border-color: red;}")))
+    ,tags$style(HTML(paste0(".panel-body {background-color: ",palette$side.panel.bg,";}")))
     
     #bttn
     ,tags$style(HTML(paste0("
@@ -206,12 +211,12 @@ getStyle <- function(){
     #topbar
     ,tags$style(HTML('#logo-id {position: fixed;right:  25px;top: 3px;}'))
     # ,tags$style(HTML('#title-id{position: fixed;right: 25vw;top: 15px;}'))    
-   
+    
     #materialSwitch
     ,tags$style(HTML(paste0('.material-switch > input[type="checkbox"]:checked + label::before
     ,.material-switch > input[type="checkbox"]:checked + label::after {
       background-color: ',palette$fg.0,';}')))
-     
+    
     ,tags$style(HTML(paste0('.material-switch > input[type="checkbox"] + label::before
     ,.material-switch > input[type="checkbox"] + label::after {
       background-color: ',palette$fg.0,'35;}')))
